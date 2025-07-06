c495c870148362e5f92cdb80488c2e04a7cb3db7
lsty <lsty@DESKTOP-9Q2FJNJ>
2025-07-07 01:47:36 +0800

    完成key和led测试任务

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * File Name          : freertos.c
-  * Description        : Code for freertos applications
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -50,23 +50,15 @@
 /* USER CODE END Variables */
 /* Definitions for LEDTask */
 osThreadId_t LEDTaskHandle;
-const osThreadAttr_t LEDTask_attributes = {
-  .name = "LEDTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
-};
+const osThreadAttr_t LEDTask_attributes = { .name = "LEDTask", .stack_size = 128
+		* 4, .priority = (osPriority_t) osPriorityNormal, };
 /* Definitions for KEYTask */
 osThreadId_t KEYTaskHandle;
-const osThreadAttr_t KEYTask_attributes = {
-  .name = "KEYTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
-};
+const osThreadAttr_t KEYTask_attributes = { .name = "KEYTask", .stack_size = 128
+		* 4, .priority = (osPriority_t) osPriorityLow, };
 /* Definitions for Event01 */
 osEventFlagsId_t Event01Handle;
-const osEventFlagsAttr_t Event01_attributes = {
-  .name = "Event01"
-};
+const osEventFlagsAttr_t Event01_attributes = { .name = "Event01" };
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -79,99 +71,103 @@ void AppKEYTask(void *argument);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /* USER CODE BEGIN PREPOSTSLEEP */
-__weak void PreSleepProcessing(uint32_t *ulExpectedIdleTime)
-{
-/* place for user code */
+__weak void PreSleepProcessing(uint32_t *ulExpectedIdleTime) {
+	/* place for user code */
 }
 
-__weak void PostSleepProcessing(uint32_t *ulExpectedIdleTime)
-{
-/* place for user code */
+__weak void PostSleepProcessing(uint32_t *ulExpectedIdleTime) {
+	/* place for user code */
 }
 /* USER CODE END PREPOSTSLEEP */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
 void MX_FREERTOS_Init(void) {
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
+	/* USER CODE BEGIN RTOS_MUTEX */
+	/* add mutexes, ... */
+	/* USER CODE END RTOS_MUTEX */
 
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
+	/* USER CODE BEGIN RTOS_SEMAPHORES */
+	/* add semaphores, ... */
+	/* USER CODE END RTOS_SEMAPHORES */
 
-  /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
+	/* USER CODE BEGIN RTOS_TIMERS */
+	/* start timers, add new ones, ... */
+	/* USER CODE END RTOS_TIMERS */
 
-  /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
+	/* USER CODE BEGIN RTOS_QUEUES */
+	/* add queues, ... */
+	/* USER CODE END RTOS_QUEUES */
 
-  /* Create the thread(s) */
-  /* creation of LEDTask */
-  LEDTaskHandle = osThreadNew(AppLEDTask, NULL, &LEDTask_attributes);
+	/* Create the thread(s) */
+	/* creation of LEDTask */
+	LEDTaskHandle = osThreadNew(AppLEDTask, NULL, &LEDTask_attributes);
 
-  /* creation of KEYTask */
-  KEYTaskHandle = osThreadNew(AppKEYTask, NULL, &KEYTask_attributes);
+	/* creation of KEYTask */
+	KEYTaskHandle = osThreadNew(AppKEYTask, NULL, &KEYTask_attributes);
 
-  /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
-  /* USER CODE END RTOS_THREADS */
+	/* USER CODE BEGIN RTOS_THREADS */
+	/* add threads, ... */
+	/* USER CODE END RTOS_THREADS */
 
-  /* Create the event(s) */
-  /* creation of Event01 */
-  Event01Handle = osEventFlagsNew(&Event01_attributes);
+	/* Create the event(s) */
+	/* creation of Event01 */
+	Event01Handle = osEventFlagsNew(&Event01_attributes);
 
-  /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
+	/* USER CODE BEGIN RTOS_EVENTS */
+	/* add events, ... */
+	/* USER CODE END RTOS_EVENTS */
 
 }
 
 /* USER CODE BEGIN Header_AppLEDTask */
 /**
-  * @brief  Function implementing the LEDTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the LEDTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_AppLEDTask */
-void AppLEDTask(void *argument)
-{
-  /* USER CODE BEGIN AppLEDTask */
-  /* Infinite loop */
-  for(;;)
-  {
+void AppLEDTask(void *argument) {
+	/* USER CODE BEGIN AppLEDTask */
+	/* Infinite loop */
+	for (;;) {
+		if(ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(500)))
+		{
+			HAL_GPIO_TogglePin(BLUE_GPIO_Port, BLUE_Pin);
 
-    osDelay(1);
-  }
-  /* USER CODE END AppLEDTask */
+		}
+		osDelay(1);
+	}
+	/* USER CODE END AppLEDTask */
 }
 
 /* USER CODE BEGIN Header_AppKEYTask */
 /**
-* @brief Function implementing the KEYTask thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the KEYTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_AppKEYTask */
-void AppKEYTask(void *argument)
-{
-  /* USER CODE BEGIN AppKEYTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END AppKEYTask */
+void AppKEYTask(void *argument) {
+	/* USER CODE BEGIN AppKEYTask */
+	/* Infinite loop */
+	for (;;) {
+		if(HAL_GPIO_ReadPin(KEY1_GPIO_Port, KEY1_Pin)==GPIO_PIN_RESET)
+		{
+			xTaskNotifyGive(LEDTaskHandle);
+			vTaskDelay(500);
+		}
+
+		osDelay(1);
+	}
+	/* USER CODE END AppKEYTask */
 }
 
 /* Private application code --------------------------------------------------*/
