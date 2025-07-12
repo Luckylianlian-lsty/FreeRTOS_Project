78f90d855b733dd2be2c8a3d3e80b5ef142627b8
lsty <lsty@DESKTOP-9Q2FJNJ>
2025-07-12 09:55:46 +0800

    实现通过串口接收数据，存入流缓冲区。再通过按键和串口发送出去

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * File Name          : freertos.c
-  * Description        : Code for freertos applications
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -29,6 +29,7 @@
 #include"queue.h"
 #include"stream_buffer.h"
 #include"usart.h"
+#include"string.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -49,21 +50,17 @@
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
 StreamBufferHandle_t StreamBufferHandle;
+extern uint8_t rxbuffer[50];
+
 /* USER CODE END Variables */
 /* Definitions for UARTTask */
 osThreadId_t UARTTaskHandle;
-const osThreadAttr_t UARTTask_attributes = {
-  .name = "UARTTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
-};
+const osThreadAttr_t UARTTask_attributes = { .name = "UARTTask", .stack_size =
+		128 * 4, .priority = (osPriority_t) osPriorityNormal, };
 /* Definitions for KeyTask */
 osThreadId_t KeyTaskHandle;
-const osThreadAttr_t KeyTask_attributes = {
-  .name = "KeyTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
-};
+const osThreadAttr_t KeyTask_attributes = { .name = "KeyTask", .stack_size = 128
+		* 4, .priority = (osPriority_t) osPriorityLow, };
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -76,94 +73,110 @@ void KeyTaskApp(void *argument);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
 void MX_FREERTOS_Init(void) {
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
+	/* USER CODE BEGIN RTOS_MUTEX */
+	/* add mutexes, ... */
+	/* USER CODE END RTOS_MUTEX */
 
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
+	/* USER CODE BEGIN RTOS_SEMAPHORES */
+	/* add semaphores, ... */
+	/* USER CODE END RTOS_SEMAPHORES */
 
-  /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
+	/* USER CODE BEGIN RTOS_TIMERS */
+	/* start timers, add new ones, ... */
+	/* USER CODE END RTOS_TIMERS */
 
-  /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
+	/* USER CODE BEGIN RTOS_QUEUES */
+	/* add queues, ... */
+	/* USER CODE END RTOS_QUEUES */
 
-  /* Create the thread(s) */
-  /* creation of UARTTask */
-  UARTTaskHandle = osThreadNew(AppUARTTask, NULL, &UARTTask_attributes);
+	/* Create the thread(s) */
+	/* creation of UARTTask */
+	UARTTaskHandle = osThreadNew(AppUARTTask, NULL, &UARTTask_attributes);
 
-  /* creation of KeyTask */
-  KeyTaskHandle = osThreadNew(KeyTaskApp, NULL, &KeyTask_attributes);
+	/* creation of KeyTask */
+	KeyTaskHandle = osThreadNew(KeyTaskApp, NULL, &KeyTask_attributes);
 
-  /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
-  StreamBufferHandle=xStreamBufferCreate(20,1);
-  /* USER CODE END RTOS_THREADS */
+	/* USER CODE BEGIN RTOS_THREADS */
+	/* add threads, ... */
+	StreamBufferHandle = xStreamBufferCreate(20, 1);
+	/* USER CODE END RTOS_THREADS */
 
-  /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
+	/* USER CODE BEGIN RTOS_EVENTS */
+	/* add events, ... */
+	/* USER CODE END RTOS_EVENTS */
 
 }
 
 /* USER CODE BEGIN Header_AppUARTTask */
 /**
-  * @brief  Function implementing the UARTTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the UARTTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_AppUARTTask */
-void AppUARTTask(void *argument)
-{
-  /* USER CODE BEGIN AppUARTTask */
-  /* Infinite loop */
-  for(;;)
-  {
-
-    osDelay(1);
-  }
-  /* USER CODE END AppUARTTask */
+void AppUARTTask(void *argument) {
+	/* USER CODE BEGIN AppUARTTask */
+	/* Infinite loop */
+	for (;;) {
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		char temp[20] = { };
+		xStreamBufferReceive(StreamBufferHandle, temp, 20, portMAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t*)temp, strlen(temp), 100);
+		vTaskDelay(200);
+	}
+	/* USER CODE END AppUARTTask */
 }
 
 /* USER CODE BEGIN Header_KeyTaskApp */
 /**
-* @brief Function implementing the KeyTask thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the KeyTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_KeyTaskApp */
-void KeyTaskApp(void *argument)
-{
-  /* USER CODE BEGIN KeyTaskApp */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END KeyTaskApp */
+void KeyTaskApp(void *argument) {
+	/* USER CODE BEGIN KeyTaskApp */
+	/* Infinite loop */
+	for (;;) {
+		if (HAL_GPIO_ReadPin(KEY1_GPIO_Port, KEY1_Pin) == GPIO_PIN_RESET) {
+			vTaskDelay(10);
+			if (HAL_GPIO_ReadPin(KEY1_GPIO_Port, KEY1_Pin) == GPIO_PIN_RESET) {
+				uint8_t len=xStreamBufferBytesAvailable(StreamBufferHandle);
+				if(len>0)
+				{
+					xTaskNotifyGive(UARTTaskHandle);
+				}
+
+			}
+		}
+		vTaskDelay(200);
+	}
+	/* USER CODE END KeyTaskApp */
 }
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-	if(huart==huart2)
-	{
-		uint8_t temp[20];
-		xStreamBufferSendFromISR(StreamBufferHandle, pvTxData, xDataLengthBytes, pxHigherPriorityTaskWoken)
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	if (huart->Instance == huart2.Instance) {
+
+		BaseType_t HigherPriorityTaskWoken = pdFALSE;
+		if (StreamBufferHandle != NULL) {
+			xStreamBufferSendFromISR(StreamBufferHandle, rxbuffer, 50,
+					&HigherPriorityTaskWoken);
+			portYIELD_FROM_ISR(HigherPriorityTaskWoken);
+
+		}
+		HAL_UART_Receive_IT(huart, rxbuffer, 4);
+
 	}
 }
 /* USER CODE END Application */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -91,7 +91,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-  HAL_UART_Receive_IT(&huart2, rxbuffer, sizeof(rxbuffer));
+  HAL_UART_Receive_IT(&huart2, rxbuffer, 4);
   /* USER CODE END 2 */
 
   /* Init scheduler */
