f9626f7d6e772b6be78da1c9d97b859e9e9fe94e
lsty <lsty@DESKTOP-9Q2FJNJ>
2025-07-12 11:13:59 +0800

    完成软件定时器的使用

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Blue_Pin GPIO_PIN_6
+#define Blue_GPIO_Port GPIOA
+#define Key1_Pin GPIO_PIN_12
+#define Key1_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * File Name          : freertos.c
-  * Description        : Code for freertos applications
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -27,6 +27,10 @@
 /* USER CODE BEGIN Includes */
 #include"event_groups.h"
 #include"queue.h"
+#include"timers.h"
+#include"stdio.h"
+#include"string.h"
+#include"usart.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -50,11 +54,14 @@
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
-};
+const osThreadAttr_t defaultTask_attributes = { .name = "defaultTask",
+		.stack_size = 128 * 4, .priority = (osPriority_t) osPriorityNormal, };
+/* Definitions for TimerPeriod */
+osTimerId_t TimerPeriodHandle;
+const osTimerAttr_t TimerPeriod_attributes = { .name = "TimerPeriod" };
+/* Definitions for TimerOnce */
+osTimerId_t TimerOnceHandle;
+const osTimerAttr_t TimerOnce_attributes = { .name = "TimerOnce" };
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -62,65 +69,102 @@ const osThreadAttr_t defaultTask_attributes = {
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
+void TimerPeriodCallback(void *argument);
+void TimerOnceCallback(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
 void MX_FREERTOS_Init(void) {
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
+	/* USER CODE BEGIN RTOS_MUTEX */
+	/* add mutexes, ... */
+	/* USER CODE END RTOS_MUTEX */
 
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
+	/* USER CODE BEGIN RTOS_SEMAPHORES */
+	/* add semaphores, ... */
+	/* USER CODE END RTOS_SEMAPHORES */
 
-  /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
+	/* Create the timer(s) */
+	/* creation of TimerPeriod */
+	TimerPeriodHandle = osTimerNew(TimerPeriodCallback, osTimerPeriodic, NULL,
+			&TimerPeriod_attributes);
 
-  /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
+	/* creation of TimerOnce */
+	TimerOnceHandle = osTimerNew(TimerOnceCallback, osTimerOnce, NULL,
+			&TimerOnce_attributes);
 
-  /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+	/* USER CODE BEGIN RTOS_TIMERS */
+	/* start timers, add new ones, ... */
+	xTimerChangePeriod(TimerOnceHandle, 3000, 100);
+	xTimerChangePeriod(TimerPeriodHandle, 2000, 100);
+	xTimerStart(TimerPeriodHandle, 100);
+	/* USER CODE END RTOS_TIMERS */
 
-  /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
-  /* USER CODE END RTOS_THREADS */
+	/* USER CODE BEGIN RTOS_QUEUES */
+	/* add queues, ... */
+	/* USER CODE END RTOS_QUEUES */
 
-  /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
+	/* Create the thread(s) */
+	/* creation of defaultTask */
+	defaultTaskHandle = osThreadNew(StartDefaultTask, NULL,
+			&defaultTask_attributes);
+
+	/* USER CODE BEGIN RTOS_THREADS */
+	/* add threads, ... */
+	/* USER CODE END RTOS_THREADS */
+
+	/* USER CODE BEGIN RTOS_EVENTS */
+	/* add events, ... */
+	/* USER CODE END RTOS_EVENTS */
 
 }
 
 /* USER CODE BEGIN Header_StartDefaultTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
-  /* USER CODE BEGIN StartDefaultTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END StartDefaultTask */
+void StartDefaultTask(void *argument) {
+	/* USER CODE BEGIN StartDefaultTask */
+	/* Infinite loop */
+	for (;;) {
+		if (HAL_GPIO_ReadPin(Key1_GPIO_Port, Key1_Pin) == GPIO_PIN_RESET) {
+			vTaskDelay(10);
+			if (HAL_GPIO_ReadPin(Key1_GPIO_Port, Key1_Pin) == GPIO_PIN_RESET) {
+				xTimerStart(TimerOnceHandle, 100);
+			}
+
+		}
+		vTaskDelay(100);
+	}
+	/* USER CODE END StartDefaultTask */
+}
+
+/* TimerPeriodCallback function */
+void TimerPeriodCallback(void *argument) {
+	/* USER CODE BEGIN TimerPeriodCallback */
+	char temp[] = "Period\n";
+	HAL_UART_Transmit(&huart2, (uint8_t*) temp, strlen(temp), 100);
+	HAL_GPIO_TogglePin(Blue_GPIO_Port, Blue_Pin);
+	/* USER CODE END TimerPeriodCallback */
+}
+
+/* TimerOnceCallback function */
+void TimerOnceCallback(void *argument) {
+	/* USER CODE BEGIN TimerOnceCallback */
+	char temp[] = "Once\n";
+	HAL_UART_Transmit(&huart2, (uint8_t*) temp, strlen(temp), 100);
+	/* USER CODE END TimerOnceCallback */
 }
 
 /* Private application code --------------------------------------------------*/
